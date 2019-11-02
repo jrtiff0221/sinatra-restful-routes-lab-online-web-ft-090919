8772f095ae78a4d9d8001c48f397517606d2716f
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-11-02 19:49:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,41 +5,41 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes' do
-      @recipes = Recipe.all
-      erb :recipes
-    end
+    @recipes = Recipe.all
+    erb :recipes
+  end
 
-    post '/recipes' do
-      @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-      redirect "/recipes/#{@recipe.id}"
-    end
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
 
-    get '/recipes/new' do
-      erb :recipe_new
-    end
+  get '/recipes/new' do
+    erb :recipe_new
+  end
 
-    get '/recipes/:id' do
-      @recipe = Recipe.find_by(:id => params[:id])
-      erb :recipe
-    end
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(:id => params[:id])
+    erb :recipe
+  end
 
-    get '/recipes/:id/edit' do
-      @recipe = Recipe.find_by(:id => params[:id])
-      erb :recipe_edit
-    end
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(:id => params[:id])
+    erb :recipe_edit
+  end
 
-    delete '/recipes/:id' do
-      @recipe = Recipe.find_by(:id => params[:id])
-      @recipe.destroy
-      redirect '/recipes'
-    end
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by(:id => params[:id])
+    @recipe.destroy
+    redirect '/recipes'
+  end
 
-    patch '/recipes/:id' do
-      @recipe = Recipe.find_by(:id => params[:id])
-      @recipe.name = params[:name]
-      @recipe.ingredients = params[:ingredients]
-      @recipe.cook_time = params[:cook_time]
-      @recipe.save
-      redirect "recipes/#{@recipe.id}"
-    end
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by(:id => params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "recipes/#{@recipe.id}"
+  end
 end
