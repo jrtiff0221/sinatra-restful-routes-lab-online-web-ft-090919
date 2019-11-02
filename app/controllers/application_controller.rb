f79415fa680b0747624f61039763e8d6f0c07428
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-11-02 19:31:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,24 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do
+    redirect to '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipes.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+  @recipes = Recipes.new
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipes = Recipes.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+
+  end
 
 end
