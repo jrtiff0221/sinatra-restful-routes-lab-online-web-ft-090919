3cf19bed0b9dbd30c33f64bcc1891a5ef94cef45
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-11-02 19:54:42 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191102183918) do
+ActiveRecord::Schema.define(version: 20191102195310) do
+
+  create_table "recipe", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.string "cook_time"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
